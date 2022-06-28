846dac60ae911ef7f471d7e9d27802e8bce5331e
isshosng <purehug@hanmail.net>
2022-06-28 16:38:39 +0900

    오류발생

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,10 @@
-import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
-import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-      <p> Hi hello nice to meet you thx a lot</p>
-    </Layout>
+    <main>
+      <title>Home Page</title>
+      <h1>Home page</h1>
+    </main>
   );
 };
-export default IndexPage;
