ba8fb5b4ee4b43bc88b63cc4e012c8c033764010
isshosng <purehug@hanmail.net>
2022-06-28 17:16:29 +0900

    테스트 페이지 추가

---
+++ src/pages/test-space.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TestSpacePage() {
+    return (
+        <main>
+            <title>Test-Space</title>
+            <h1>This is Page of Test-Space</h1>
+        </main>
+    );
+}
+
+export default TestSpacePage;
